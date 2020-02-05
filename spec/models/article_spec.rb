791a81a1901ef223d1660cbed90f07ba7eade18c
Oscar M Olivera <oscarmolivera@gmail.com>
2020-02-05 20:20:21 -0300

    Primeras pruebas TDD simples sobre Aricles

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#validations" do
+    let(:article) { create(:article) }
+    
+    it "should test that the factory is valid" do
+      expect(article).to be_valid
+    end
+  
+    it "should validate the precence of the title" do
+      article.title =  ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:title]).to include("can't be blank")
+    end
+    
+  
+    it "should validate the precence of the content" do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:content]).to include("can't be blank")
+    end
+    
+    it "should validate the precence of the slug" do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+    end
+  
+    it "should validate slug uniqueness" do
+      #article2 = create(:article)
+      #invalid_article = FactoryBot.build :article, slug: article.slug
+      invalid_article = build :article, slug: article.slug
+      expect(invalid_article).not_to be_valid      
+    end
+  end
+  
 end
