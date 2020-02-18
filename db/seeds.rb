8bb9469201494277f789864a649d04a289ed4fa8
Oscar M Olivera <oscarmolivera@gmail.com>
2020-02-18 16:00:35 -0300

    Archivo Seed corregido

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,18 @@
+user = User.create(
+  login: 'SeedUser01',
+  name: 'seeding user',
+  url: 'http://seeding-url.com',
+  avatar_url: 'http://gravitar.com/seeding-user',
+  provider: 'Github'
+)
 (1..25).each_with_index do |article, index|
   Article.create!(
     {
-      "title" => "TITULO Articulo N°#{index+1}",
-      "content" => "Contenido #{index+1}: Lorem Ipsum ...",
-      "slug" => "page-articulo-#{index+1}"
+      'title' => "TITULO Articulo N°#{index + 1}",
+      'content' => "Contenido #{index + 1}: Lorem Ipsum ...",
+      'slug' => "page-articulo-#{index + 1}",
+      'user_id' => user.id
     }
   )
 end
-puts "25 Articulos creados!"
+puts '25 Articulos creados!'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    sequence (:login) { |l| "usuario.prueba#{l}" }
-    name { "Usuario de Prueba" }
-    url { "http://example-url.com" }
-    avatar_url { "http://gravitar.com/oolivera" }
-    provider { "Github" }
+    sequence(:login) { |l| "usuario.prueba#{l}" }
+    name { 'Usuario de Prueba' }
+    url { 'http://example-url.com' }
+    avatar_url { 'http://gravitar.com/oolivera' }
+    provider { 'Github' }
   end
 end
